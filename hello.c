56792afc3b06503a96a4e3ba12b35db307f6c8fe
Jace Miller <jmiller111@huskers.unl.edu>
2020-08-21 18:32:28 +0000

    added hello.c file

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jace Miller
+ * Date: 2020
+ *
+ * A simple hello world program in C
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Jace Miller\n");
+    printf("Undecided\n");
+
+    return 0;
+}
